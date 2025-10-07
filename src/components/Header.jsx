97440538cc8dd59dcf21e88c29dfdd1dc9295e6c
Tomi <tomascherasco60@gmail.com>
2025-10-06 21:22:25 -0300

    Second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header({ cartCount, onToggleCart }) {
     <header className="header">
       <div className="header-content">
         <div className="logo">
-          <img src="/img/Logo de ZonaRebote.png" alt="Logo ZonaRebote" />
+          <img src="https://raw.githubusercontent.com/tomascherasco60-sketch/ZonaRebote/refs/heads/main/public/img/Logo%20de%20ZonaRebote.png" alt="Logo ZonaRebote" />
           ZONAREBOTE
         </div>
         <nav>
@@ -15,7 +15,7 @@ export default function Header({ cartCount, onToggleCart }) {
           </ul>
         </nav>
         <div className="cart-icon" onClick={onToggleCart}>
-          <img src="/img/cart.svg" alt="Carrito" className="cart-logo" />
+          <img src="https://raw.githubusercontent.com/tomascherasco60-sketch/ZonaRebote/b324ba05d4a7213a95d9c0b72f61aec95823c458/public/img/cart.svg" alt="Carrito" className="cart-logo" />
           <span className="cart-count">{cartCount}</span>
         </div>
       </div>
